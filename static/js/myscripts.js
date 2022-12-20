ed9d08709e36fd9929efc12f3d5b68f102a92320
Halyna Kravchuk <hkravchuk@rework-space.com>
2022-12-20 19:16:00 +0200

    layouts were improved

--- static/js/myscripts.js
+++ static/js/myscripts.js
@@ -1,10 +1,20 @@
+// When the user scroll down, header will be fixed on the screen top
+// and a "scroll to top" button will be shown
 window.onscroll = function(){
     let top = window.scrollY;
-    console.log('Top: ' + top);
     let header = document.getElementsByTagName('nav');
+    let btnBackToTop = document.getElementById("btnBackToTop");
     if(top > 100){
         header[0].classList.add('image-header');
+        btnBackToTop.style.display = "block";
     } else {
         header[0].classList.remove('image-header');
+        btnBackToTop.style.display = "none";
     }
 };
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
